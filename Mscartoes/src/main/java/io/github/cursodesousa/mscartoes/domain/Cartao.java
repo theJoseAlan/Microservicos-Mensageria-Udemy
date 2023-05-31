778c070ecf673ba13fb1208d4768643ddf8a7f80
theJoseAlan <salesalan854@gmail.com>
2023-05-31 12:25:20 -0300

    Create Cartao.java

---
+++ Mscartoes/src/main/java/io/github/cursodesousa/mscartoes/domain/Cartao.java
@@ -0,0 +1,30 @@
+package io.github.cursodesousa.mscartoes.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cartao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    /* Essa anotação indica o tipo de dado a ser salvo a partir da ENUM
+    * Se for Ordinal, ao invés de String, ele salva a primeira enum como 0, a segunda como 1 e assim vai
+    * */
+    @Enumerated(EnumType.STRING)
+    private BandeiraCartao bandeira;
+
+    private BigDecimal renda;
+
+    private BigDecimal limiteBasico;
+
+}
