621e0aca2971dd294960d8e6c97dcbe2055e6a78
theJoseAlan <salesalan854@gmail.com>
2023-06-01 14:14:12 -0300

    Create CartoesPorClienteResponse.java

---
+++ Mscartoes/src/main/java/io/github/cursodesousa/mscartoes/application/representation/CartoesPorClienteResponse.java
@@ -0,0 +1,28 @@
+package io.github.cursodesousa.mscartoes.application.representation;
+
+import io.github.cursodesousa.mscartoes.domain.ClienteCartao;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartoesPorClienteResponse {
+
+    private String nome;
+
+    private String bandeira;
+
+    private BigDecimal limiteLiberado;
+
+    public static CartoesPorClienteResponse fromModel(ClienteCartao model){
+        return new CartoesPorClienteResponse(
+                model.getCartao().getNome(),
+                model.getCartao().getBandeira().toString(),
+                model.getLimite()
+        );
+    }
+}
