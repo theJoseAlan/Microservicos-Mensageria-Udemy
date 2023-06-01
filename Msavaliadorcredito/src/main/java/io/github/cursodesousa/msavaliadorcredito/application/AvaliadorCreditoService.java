4d2da9d27b4df177871ae5d2ea235d9e6a2304cc
theJoseAlan <salesalan854@gmail.com>
2023-06-01 15:00:11 -0300

    Create AvaliadorCreditoService.java

---
+++ Msavaliadorcredito/src/main/java/io/github/cursodesousa/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -0,0 +1,16 @@
+package io.github.cursodesousa.msavaliadorcredito.application;
+
+import io.github.cursodesousa.msavaliadorcredito.domain.model.SituacaoCliente;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AvaliadorCreditoService {
+
+    public SituacaoCliente obterSituacaoCliente(){
+        //Obter dados de msClientes
+        //Obter cartoes do cliente de msCartoes
+    }
+
+}
