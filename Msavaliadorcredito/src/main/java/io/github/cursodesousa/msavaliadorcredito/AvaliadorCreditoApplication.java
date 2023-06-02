0fc3b2bdbfa0cfb5c8b79aa8fb09af6b16ffc805
theJoseAlan <salesalan854@gmail.com>
2023-06-02 08:43:36 -0300

    add @EnableFeignClients

--- Msavaliadorcredito/src/main/java/io/github/cursodesousa/msavaliadorcredito/AvaliadorCreditoApplication.java
+++ Msavaliadorcredito/src/main/java/io/github/cursodesousa/msavaliadorcredito/AvaliadorCreditoApplication.java
@@ -3,9 +3,11 @@ package io.github.cursodesousa.msavaliadorcredito;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients //Scaneia onde tem feign clients e dá implementação a elas
 public class AvaliadorCreditoApplication {
 
 	public static void main(String[] args) {
